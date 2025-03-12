d0031768ace87828aa96a5c5d25f4117f79567f3
Rohith Nair <rohithnair2910@gmail.com>
2025-03-12 16:27:03 +0530

    Added assets files along with few images

--- app/layout.js
+++ app/layout.js
@@ -20,9 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        // className={'font-grobold'}
-      >
+      <body>
         <ApolloContext>
           {children}
         </ApolloContext>
