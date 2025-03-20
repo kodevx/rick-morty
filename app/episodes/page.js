60f8974142c76d8f05b7e2afc4af7f0414fcb994
Rohith Nair <rohithnair2910@gmail.com>
2025-03-20 10:59:07 +0530

    Added Episode Page

---
+++ app/episodes/page.js
@@ -0,0 +1,66 @@
+'use client'
+
+import LocationCards from "@/components/LocationCards";
+import CardsShimmer from "@/components/Shimmers/cardsShimmer";
+import Pagination from "@/components/Pagination";
+import useEpisodes from "@/customHooks/Episodes/useEpisodes";
+
+import GET_ALL_EPISODES_QUERY from '@/queries/getAllLocations.graphql';
+
+const Episodes = (props) => {
+
+    const {
+      locations,
+      handleNext, 
+      handlePrev,
+      totalCount,
+      isCalled,
+      isLoading,
+      paginationState,
+      handlePageUpdate,
+    } = useEpisodes({
+      queries: { 
+        getAllEpisodessQuery: GET_ALL_EPISODES_QUERY
+      }
+    });
+
+  if(isCalled && locations.length === 0) { 
+    <div className="flex flex-col justify-center mt-5 items-center">
+      No Locations Found
+    </div>
+  }
+
+  return (
+    <div>
+      <main>
+        <div className="flex flex-col font-grobold mt-5">
+          <div className="text-2xl self-center tracking-wider">
+            All Locations <span className="text-green-600">({`${totalCount ? totalCount : ''}`})</span>
+          </div>
+          <div className="px-20 py-5 flex flex-col justify-center items-center">
+            <div className="grid grid-cols-2 gap-5 justify-between">
+              {isLoading || !locations 
+                ? <CardsShimmer /> 
+                : locations && locations.length > 0 && (
+                    locations.map(location => (
+                      <LocationCards key={location.id} {...location} />
+                    ))
+                  )
+              }
+            </div>
+            <div className="py-10">
+              <Pagination 
+                  {...paginationState}
+                  handlePageUpdate={handlePageUpdate}
+                  handleNext={handleNext}
+                  handlePrev={handlePrev}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Episodes;
