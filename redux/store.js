3ea1447b88e150a7ac87709e25450d22904366a3
Rohith Nair <rohithnair2910@gmail.com>
2025-03-21 14:17:17 +0530

    Added redux-persist to retain date on stores on page reloads

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,42 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
+import { combineReducers } from "@reduxjs/toolkit";
 import favouritesReducer from './reducers/favourites';
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    persistStore, 
+    persistReducer,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootPersistConfig = {
+    key: 'root',
+    storage,
+}
+
+// Add Other Reducers in future cases
+const rootReducers = combineReducers({  
+    favourites: favouritesReducer
+})
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducers);
 
 export const store = configureStore({
-    reducer: {
-        favourites: favouritesReducer
-    }
+    reducer:  persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
 })
+
+export const persistor = persistStore(store);
 
 export const useAppDispatch = useDispatch.withTypes();
 export const useAppSelector = useSelector.withTypes();
